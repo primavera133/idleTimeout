df64f3e0524ccc19ca91bc631ebc7444c16f4cb7
Jonas Myrenås <jonas@myrenas.se>
2011-05-03 15:08:39 +0200

    changed to utf-8 encoding

---
+++ idleTimeout.js
@@ -0,0 +1,114 @@
+//######
+//## This work is licensed under the Creative Commons Attribution-Share Alike 3.0
+//## United States License. To view a copy of this license,
+//## visit http://creativecommons.org/licenses/by-sa/3.0/us/ or send a letter
+//## to Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
+//######
+
+/*
+This jQuery plugin is an extended version of Phil Palmieris work; http://philpalmieri.com/2009/09/jquery-session-auto-timeout-with-prompt/
+
+My extension is listening to the jQuery event ajaxSend so that the timeout is reset when other ajax requests are sent (assuming they will reset the session timeout on the server)
+
+/Jonas Myrenås, jonas@myrenas.se
+*/
+
+(function($) {
+    $.fn.idleTimeout = function(options) {
+        var defaults = {
+            inactivity: 1200000, //20 Minutes
+            noconfirm: 10000, //10 Seconds
+            sessionAlive: 30000, //10 Minutes
+            redirect_url: '/js_sandbox/',
+            click_reset: true,
+            alive_url: '/js_sandbox/',
+            logout_url: '/js_sandbox/',
+            useAjaxSend: false
+
+        };
+
+        //##############################
+        //## Private Variables
+        //##############################
+        var opts = $.extend(defaults, options);
+        var liveTimeout, confTimeout, sessionTimeout;
+        var modal = "<div id='modal_pop'><p>You are about to be signed out due to inactivity.</p></div>";
+        //##############################
+        //## Private Functions
+        //##############################
+        var start_liveTimeout = function() {
+            clearTimeout(liveTimeout);
+            clearTimeout(confTimeout);
+            liveTimeout = setTimeout(logout, opts.inactivity);
+
+            if (opts.sessionAlive) {
+                clearTimeout(sessionTimeout);
+                sessionTimeout = setTimeout(keep_session, opts.sessionAlive);
+            }
+        };
+
+        var logout = function() {
+
+            confTimeout = setTimeout(redirect, opts.noconfirm);
+            $(modal).dialog({
+                buttons: {"Stay Logged In":  function() {
+                    $(this).dialog('close');
+                    stay_logged_in();
+                }},
+                modal: true,
+                title: 'Auto Logout'
+            });
+
+        };
+
+        var redirect = function() {
+            if (opts.logout_url) {
+                $.get(opts.logout_url);
+            }
+            window.location.href = opts.redirect_url;
+        };
+
+        var stay_logged_in = function(el) {
+            start_liveTimeout();
+            if (opts.alive_url) {
+                $.get(opts.alive_url);
+            }
+        };
+
+        var keep_session = function() {
+            opts.useAjaxSend = false;
+
+            $.get(opts.alive_url);
+            clearTimeout(sessionTimeout);
+            sessionTimeout = setTimeout(keep_session, opts.sessionAlive);
+
+            opts.useAjaxSend = true;
+        };
+
+        //Bind keep_alive to ajaxSend-event, so any ajax event resets the timer
+        this.ajaxSend(function(){
+            if(opts.useAjaxSend){
+                //console.log("ajaxSend triggered");
+                keep_session();
+            }
+        });
+
+        //###############################
+        //Build & Return the instance of the item as a plugin
+        // This is basically your construct.
+        //###############################
+        return this.each(function() {
+            obj = $(this);
+
+            start_liveTimeout();
+            if (opts.click_reset) {
+                $(document).bind('click', start_liveTimeout);
+            }
+            if (opts.sessionAlive) {
+                keep_session();
+            }
+            opts.useAjaxSend = true;
+        });
+
+    };
+}(jQuery));
